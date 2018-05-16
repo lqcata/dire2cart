974f14d04010922793b6ff4f4299d811a164c4de
lqcata <lqcata@gmail.com>
2018-05-16 13:22:15 +0200

    Update dire2cart.py

--- dire2cart.py
+++ dire2cart.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #Convert direc coordiation to cartesian Writen By Qiang 
-import subprocess
+import sys
 
-subprocess.call('rm POSCAR_C -f', shell = True)
+script, file_to_be_converted = sys.argv 
 
-file_read = open('POSCAR', 'r')
+print """
+###################################
+#                                 #
+#for VASP 5.2 or higher versions  #
+#                                 #
+###################################
+"""
+
+file_read = open(file_to_be_converted, 'r')
 
 line = file_read.readlines()
 a1 = float(line[2].split()[0])
@@ -25,33 +33,77 @@ y_cartesian = []
 z_cartesian = []
 tf = []
 
-start_num = 9 # With Selected T T T, coordination starts from line 9
-if 'S' not  in line[7]: 
+start_num = 9 # Default: With Selected T T T, coordination starts from line 9
+
+def convert():
+    
+    for i in range(start_num, num_atoms + start_num):
+        x_cartesian.append(float(line[i].split()[0]) * a1 + float(line[i].split()[1]) * a2 + float(line[i].split()[2]) * a3)
+        y_cartesian.append(float(line[i].split()[0]) * b1 + float(line[i].split()[1]) * b2 + float(line[i].split()[2]) * b3)
+        z_cartesian.append(float(line[i].split()[0]) * z1 + float(line[i].split()[1]) * z2 + float(line[i].split()[2]) * z3)
+        if len(line[i].split()) > 3:   # if  T T T exist, there are more than 3 elements in the list line[i].split()
+            tf.append((line[i].split()[3]))
+        else:
+            tf.append(' ')   # if there is no T T T, use space instead. 
+    
+    file_out = open(file_to_be_converted+'_C', 'w')
+    
+    for i in range(0,7):
+        file_out.write(line[i].rstrip() + '\n')  # first 7 lines are kept the same 
+    
+    if 'S' in line[7]:
+        file_out.write(line[7].rstrip()+ '\n')  # if  T T T exists, write the Selective line 
+    file_out.write('Cartesian' + '\n')          # Coordination system is Cartesian now. 
+    
+    for i in range(0,len(x_cartesian)):
+        file_out.write("%+-3.10f   %+-3.10f   %+-3.10f   %s %s %s\n"  
+        %(x_cartesian[i], y_cartesian[i], z_cartesian[i], tf[i], tf[i], tf[i]))
+    
+    file_out.close()
+    print '-----------------------------------------------------\n'
+    print 'POSCAR with Cartesian Coordiations is named as %s_C\n' %(file_to_be_converted)
+    print '-----------------------------------------------------\n'
+
+if line[7][0]  == 'S' or line[7][0]  == 's':  # # With Selected T T T, coordination starts from line 9
+    start_num = 9
+
+    if  line[8][0]  == 'D' or line[8][0]  == 'd':
+        print """
+This POSCAR has Direct Coordinations, Conversion is starting....
+
+              """
+        convert()
+    
+    elif  line[8][0]  == 'C' or line[8][0]  == 'c':
+        print """
+This POSCAR has Cartesian Coordinations! Process is aborted!
+              
+              """
+
+
+else : 
+    print """
+----------------------------------------------------
+Pay Attetion! There is no TTT in coordinations part!
+----------------------------------------------------
+"""
+    
     start_num = 8 # without Selected, No  T T T , coordination starts from line 8 
+    
+    if  line[7][0]  == 'D' or line[7][0]  == 'd':
+        print """
+This POSCAR has Direct Coordinations, Contersion starts....
 
-for i in range(start_num, num_atoms + start_num):
-    x_cartesian.append(float(line[i].split()[0]) * a1 + float(line[i].split()[1]) * a2 + float(line[i].split()[2]) * a3)
-    y_cartesian.append(float(line[i].split()[0]) * b1 + float(line[i].split()[1]) * b2 + float(line[i].split()[2]) * b3)
-    z_cartesian.append(float(line[i].split()[0]) * z1 + float(line[i].split()[1]) * z2 + float(line[i].split()[2]) * z3)
-    if len(line[i].split()) > 3:   # if  T T T exist, there are more than 3 elements in the list line[i].split()
-        tf.append((line[i].split()[3]))
-    else:
-        tf.append(' ')   # if there is no T T T, use space instead. 
+"""
+        convert()
+    
+    elif  line[7][0]  == 'C' or line[7][0]  == 'c':
+        print """
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+This POSCAR has Cartesian Coordinations already! 
 
-file_out = open('POSCAR_C', 'a')
-
-for i in range(0,7):
-    file_out.write(line[i].rstrip() + '\n')  # first 7 lines are kept the same 
-
-if 'S' in line[7]:
-    file_out.write(line[7].rstrip()+ '\n')  # if  T T T exists, write the Selective line 
-file_out.write('Cartesian' + '\n')          # Coordination system is Cartesian now. 
-
-for i in range(0,len(x_cartesian)):
-    file_out.write("%+-3.10f   %+-3.10f   %+-3.10f   %s %s %s\n"  
-    %(x_cartesian[i], y_cartesian[i], z_cartesian[i], tf[i], tf[i], tf[i]))
+Process is aborted!
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
 
 file_read.close()
-file_out.close()
-
-print 'POSCAR with Cartesian Coordiations is named as POSCAR_C'
